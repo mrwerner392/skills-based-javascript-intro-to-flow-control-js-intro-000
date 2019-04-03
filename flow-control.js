9a3193a17c11307cfd1c31a2b3a9fc7d3bc68f22
Matt Werner <mrwerner392@gmail.com>
2019-04-03 15:41:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age < 13) {
+    return 'You are a kid';
+  } else if (age < 20) {
+    return 'You are a teenager!';
+  } else {
+    return 'You are a grownup';
+  }
 }
 
 function ternaryTeenager(age) {
